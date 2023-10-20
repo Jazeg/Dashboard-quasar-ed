1bbc87541027ada14c6b4c81c48b08c0cea6cfed
Jazeg <wilwaeb@hotmail.com>
2023-10-20 10:15:15 -0500

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,94 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          CITE
+        </q-toolbar-title>
+
+        <div>
+        <q-btn label="Opciones">
+  <q-menu>
+    <q-item clickable v-close-popup>
+      <q-item-section>Salir</q-item-section>
+    </q-item>
+  </q-menu>
+</q-btn>
+</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          CITE
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Registro',
+    icon: 'app_registration',
+    link: '/'
+  },
+  {
+    title: 'Seguridad',
+    icon: 'security',
+    link: '/blank'
+
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
